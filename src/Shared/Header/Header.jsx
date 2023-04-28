570a21aa1d193ad23ec07f03ed74f34f14af69b1
ShahidaAkter1 <shahida0200@gmail.com>
2023-04-28 15:30:21 +0600

    some changes add

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -5,6 +5,7 @@ import { Button, Container } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 
 
@@ -29,7 +30,9 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#features">
+              <Link to="/">Home</Link>
+            </Nav.Link>
             <Nav.Link href="#pricing">About</Nav.Link>
             <Nav.Link href="#pricing">Career</Nav.Link>
            
